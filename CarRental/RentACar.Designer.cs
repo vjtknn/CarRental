653b5c369f7af77e1caa2688af95f9e2681c42c7
Wojciech Winnicki <wbwinnicki@gmail.com>
2017-01-11 12:47:42 +0100

    Label added to RentACar form

--- CarRental/RentACar.Designer.cs
+++ CarRental/RentACar.Designer.cs
@@ -28,11 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(213, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(279, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Wypożycz samochód:";
+            // 
+            // RentACar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(754, 481);
+            this.Controls.Add(this.label1);
+            this.Name = "RentACar";
             this.Text = "RentACar";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
     }
 }
