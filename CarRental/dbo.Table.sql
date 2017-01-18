6f3bd67e708eb3b08731e79d65ec2abbba1c9ba5
xnextx <piotr@placenet.pl>
2017-01-18 10:45:37 +0100

    added script database

---
+++ CarRental/dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Cars]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Brand] VARCHAR(50) NULL, 
+    [Model] VARCHAR(50) NULL, 
+    [Seats] INT NULL, 
+    [Equipmentss] NCHAR(10) NULL
+)
